4692a1308b1abf56d90b3ec733bafb0b8f7270a4
Ponynja <ponynja@hotmail.fr>
2020-04-21 16:27:02 +0200

    v3

--- Javascript/menu/menu.js
+++ Javascript/menu/menu.js
@@ -1,21 +1,35 @@
 var labels = document.getElementsByClassName("menuLabel");
 
 for (let i = 0; i < labels.length; i++) {
-    labels[i].addEventListener("click", function() {
+    labels[i].addEventListener("click", labelClickListener);
+}
 
-        var menu = labels[i].parentElement;
+function closeAll() {
+    var active = document.getElementsByClassName("active");
 
-        if (menu.classList.contains("active")) {
-            menu.classList.remove("active");
+    for (let i = 0; i < active.length; i++) {
+        active[i].classList.remove("active");
+    }
+}
 
-            var activeMenus = menu.getElementsByClassName("active");
 
-            for (let j = 0; j < activeMenus.length; j++) {
-                activeMenus[j].classList.remove("active");
-            }
+function labelClickListener() {
 
-        } else {
-            menu.classList.add("active");
+    var menu = event.target.parentElement;
+
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+
+        var activeMenus = menu.getElementsByClassName("active");
+
+        for (let j = 0; j < activeMenus.length; j++) {
+            activeMenus[j].classList.remove("active");
         }
-    })
+
+    } else {
+        if (menu.classList.contains("category")) {
+            closeAll();
+        }
+        menu.classList.add("active");
+    }
 }
