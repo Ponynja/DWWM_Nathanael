1d9007a011012cc71b607677471796593c565dcd
Ponynja <ponynja@hotmail.fr>
2020-04-22 10:33:48 +0200

    v4

--- Javascript/menu/menu.js
+++ Javascript/menu/menu.js
@@ -1,9 +1,13 @@
+//Labels cliquables des menus et sous-menus
 var labels = document.getElementsByClassName("menuLabel");
 
+//Ajout de l'envent click sur chaque label
 for (let i = 0; i < labels.length; i++) {
     labels[i].addEventListener("click", labelClickListener);
 }
 
+
+//Fonction qui ferme tous les menus actifs
 function closeAll() {
     var active = document.getElementsByClassName("active");
 
@@ -12,24 +16,43 @@ function closeAll() {
     }
 }
 
-
+//Click Listener qui ouvre ou ferme le menu lorsque son label est cliqué
 function labelClickListener() {
 
     var menu = event.target.parentElement;
+    var parentMenu = menu.parentElement;
 
+    //Si le menu est ouvert, le fermer
     if (menu.classList.contains("active")) {
+
+        //Enlever la classe active
         menu.classList.remove("active");
 
-        var activeMenus = menu.getElementsByClassName("active");
+        //Recuperer tous les sous-menus enfants ou adjacents ouverts
+        var activeMenus = parentMenu.getElementsByClassName("active");
 
+        //Fermer tous les sous-menus
         for (let j = 0; j < activeMenus.length; j++) {
             activeMenus[j].classList.remove("active");
         }
 
+        //Si le menu est fermé, l'ouvrir
     } else {
+
+        //Si le menu est une categorie, fermer tous les autres
         if (menu.classList.contains("category")) {
             closeAll();
         }
+
+        //Recuperer tous les sous-menus enfants ou adjacents ouverts
+        var activeMenus = parentMenu.getElementsByClassName("active");
+
+        //Fermer tous les sous-menus
+        for (let j = 0; j < activeMenus.length; j++) {
+            activeMenus[j].classList.remove("active");
+        }
+
+        //Ajouter la classe active
         menu.classList.add("active");
     }
 }
