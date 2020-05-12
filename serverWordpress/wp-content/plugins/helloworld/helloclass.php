340a5a9a180068fbefc45e15f00dd1cb3847c44b
Ponynja <ponynja@hotmail.fr>
2020-05-12 10:26:18 +0200

    Wordpress Plugin

--- serverWordpress/wp-content/plugins/helloworld/helloclass.php
+++ serverWordpress/wp-content/plugins/helloworld/helloclass.php
@@ -9,6 +9,8 @@ class HelloClass
         add_action('widgets_init', function () {register_widget('HelloWidget');});
         add_action('wp_enqueue_scripts', array($this, 'persoCSS'), 15);
         add_action('wp_loaded', array($this, 'save_comm'));
+        add_action('admin_menu', array($this, 'add_admin_menu'), 20);
+        add_action('admin_init', array($this, 'register_settings'));
     }
 
     public function persoCSS()
@@ -20,25 +22,60 @@ class HelloClass
     { //méthode déclenchée à l'activation du plug-in
         global $wpdb;
         $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}helloworld_commentaire (id INT AUTO_INCREMENT PRIMARY KEY, comm VARCHAR(255) NOT NULL);");
+        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}helloworld_nom (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL);");
     }
 
     public static function uninstall()
     { //méthode déclenchée à la suppression du module
         global $wpdb;
         $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}helloworld_commentaire;");
+        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}helloworld_nom;");
     }
 
     public function save_comm()
     {
-        if (isset($_POST['helloworld_comm']) && !empty($_POST['helloworld_comm'])) {
+        // var_dump($_POST);
+        if (isset($_POST['helloworld_comm']) && !empty($_POST['helloworld_comm']) && isset($_POST['helloworld_name']) && !empty($_POST['helloworld_name'])) {
             global $wpdb;
+            $name = $_POST['helloworld_name'];
             $comm = $_POST['helloworld_comm'];
-            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}helloworld_commentaire WHERE comm = '$comm'");
+            $rowcomm = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}helloworld_commentaire WHERE comm = '$comm'");
+            $rowname = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}helloworld_nom WHERE name = '$name'");
 
-            if (is_null($row)) {
+            if (is_null($rowcomm)) {
                 $wpdb->insert("{$wpdb->prefix}helloworld_commentaire", array('comm' => $comm));
 
             }
+
+            if (is_null($rowname)) {
+                $wpdb->insert("{$wpdb->prefix}helloworld_nom", array('name' => $name));
+            }
+
         }
+    }
+
+    public function add_admin_menu()
+    { //on ajoute une page dans le menu administrateur
+        add_menu_page('Hello World', 'Hello World plugin', 'manage_options',
+
+            'helloworld', array($this, 'menu_html'));
+
+    }
+    public function menu_html()
+    {
+        echo '<h1>' . get_admin_page_title() . '</h1>';?>
+        <form method="post" action="options.php">
+            <?php settings_fields('helloworld_settings')?>
+            <label>Couleur</label>
+            <input type="text" name="helloworld_couleur" value="<?php echo get_option("helloworld_couleur") ?>"/>
+
+            <?php submit_button();?>
+        </form>
+        <?php
+    }
+
+    public function register_settings()
+    {
+        register_setting('helloworld_settings', 'helloworld_couleur');
     }
 }
--- serverWordpress/wp-content/plugins/helloworld/hellowidget.php
+++ serverWordpress/wp-content/plugins/helloworld/hellowidget.php
@@ -22,6 +22,9 @@ class hellowidget extends WP_Widget
         <p class="hellowidget">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, cupiditate dolore placeat praesentium in quidem laborum dignissimos sed sit alias maiores similique beatae veniam pariatur, soluta, dolorum fugit modi consequatur.</p>
         <form action="" method="post">
             <p>
+                <label for="helloworld_name">Votre nom :</label>
+                <input id="helloworld_name" name="helloworld_name" type="texte"/>
+
                 <label for="helloworld_comm">Votre commentaire :</label>
                 <input id="helloworld_comm" name="helloworld_comm" type="texte"/>
             </p>
@@ -29,6 +32,14 @@ class hellowidget extends WP_Widget
         </form>
 
         <?php
-echo $args['after_widget'];
+
+        $couleur = get_option('helloworld_couleur', 'white');
+        ?>
+
+            <div id="test" style="color:<?php echo $couleur; ?>">Hello World est un plug-in qui enregistre les commentaires en base de données</div>
+
+        <?php
+
+        echo $args['after_widget'];
     }
 }
